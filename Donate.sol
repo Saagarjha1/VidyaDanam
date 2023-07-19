532e7cf6b34bec58c37ff13aa8ca64350b45a065
Saagarjha1 <126740871+Saagarjha1@users.noreply.github.com>
2023-07-19 11:34:47 +0530

    Update Donate.sol

--- Donate.sol
+++ Donate.sol
@@ -1,31 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Chai {
-    struct Memo {
+contract Donation {
+    struct DonationInfo {
         string name;
         string message;
         uint256 timestamp;
         address from;
     }
 
-    Memo[] public memos;
+    DonationInfo[] public donations;
     address payable public owner;
 
-    event ChaiPurchased(address indexed buyer, string name, string message, uint256 timestamp);
+    event DonationMade(address indexed donor, string name, string message, uint256 timestamp);
 
     constructor() {
         owner = payable(msg.sender);
     }
 
-    function buyChai(string memory name, string memory message) public payable {
-        require(msg.value > 0, "Please pay greater than 0 ether");
+    function makeDonation(string memory name, string memory message) public payable {
+        require(msg.value > 0, "Please donate more than 0 ether");
         owner.transfer(msg.value);
-        memos.push(Memo(name, message, block.timestamp, msg.sender));
-        emit ChaiPurchased(msg.sender, name, message, block.timestamp);
+        donations.push(DonationInfo(name, message, block.timestamp, msg.sender));
+        emit DonationMade(msg.sender, name, message, block.timestamp);
     }
 
-    function getMemos() public view returns (Memo[] memory) {
-        return memos;
+    function getDonations() public view returns (DonationInfo[] memory) {
+        return donations;
     }
 }
